64fb7bf00bb949eefcb276246488dd60f01e7cf4
Luigi Verolla <luverolla@outlook.com>
2021-01-31 15:12:30 +0100

    File upload

---
+++ js/locales.js
@@ -0,0 +1,31 @@
+window.DT_LOCALES = {};
+
+window.DT_LOCALES["it"] =
+{
+	SEARCH: "cerca",
+	PER_PAGE: "{NUM} per pagina",
+	SHOWING_TO: "righe da {FROM} a {TO} di {SIZE}",
+	GOTO_FIRST: "vai a pagina 1",
+	GOTO_PREV: "vai a pagina precedente",
+	GOTO_PAGE: "vai a pagina {NUM}",
+	GOTO_NEXT: "vai a pagina successiva",
+	GOTO_LAST: "vai all'ultima pagina",
+	ASC_ACTIVE: "ordine crescente attivo",
+	DESC_ACTIVE: "ordine decrescente attivo",
+	NOT_ACTIVE: "non attivo"
+}
+
+window.DT_LOCALES["en"] =
+{
+	SEARCH: "search",
+	PER_PAGE: "{NUM} per page",
+	SHOWING_TO: "rows {FROM} to {TO} of {SIZE}",
+	GOTO_FIRST: "go to page 1",
+	GOTO_PREV: "go to previous page",
+	GOTO_PAGE: "go to page {NUM}",
+	GOTO_NEXT: "go to next page",
+	GOTO_LAST: "go to last page",
+	ASC_ACTIVE: "ascending order active",
+	DESC_ACTIVE: "descending order active",
+	NOT_ACTIVE: "not active"
+}
